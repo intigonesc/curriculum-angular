687a9935238af57b0905341061e276b589c2bf8e
intigonesc <flexter31@gmail.com>
2023-08-17 22:12:02 -0300

    completo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DatosPersonalesComponent } from './datos-personales/datos-personales.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { EstudiosComponent } from './estudios/estudios.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatosPersonalesComponent,
+    ContactoComponent,
+    ExperienciaComponent,
+    EstudiosComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
